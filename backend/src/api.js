d06b3395d7d25a06f685c4222a4a5b6cc16d661c
Alaa <ahaddabeh@gmail.com>
2020-06-14 00:33:21 -0400

    Updated api file to work with axios calls; updated db path in index controller file

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,106 +1,106 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export const fetchAll = async (url, params, method = "get") => {
-//     return await axios({
-//         method, url: `http://localhost:3500${url}`, params
-//     })
-// }
-
-// export const fetchOne = async (url, params = {}, method = "get") => {
-//     return await axios({
-//         method, url: `http://localhost:3500${url}`, params
-//     })
-// }
-
-
-// export const save = async (url, data, method = "post") => {
-//     return await axios({
-//         method, url: `http://localhost:3500${url}`, data,
-//         headers: {
-//             "content-type": "application/json"
-//         }
-//     })
-// }
-
-
-
-
-const data = require("../../output.json");
-
-console.log(data.content.filter(it => it.contentTypeId === 1));
-
-const getContent = (type) => data.content.filter(it => it.contentTypeId === +type);
-
-const getPages = () => getContent(1);
-const getPage = (id) => {
-    const pagesArr = getPages();
-    for (let i = 0; i < pagesArr.length; i++) {
-        if (pagesArr[i].id === +id) {
-            return pagesArr[i];
-        }
-    }
-    return {};
-}
-
-const getCategories = () => getContent(2);
-const getCategory = (id) => {
-    const categoriesArr = getCategories();
-    for (let i = 0; i < categoriesArr.length; i++) {
-        if (categoriesArr[i].id === +id) {
-            return categoriesArr[i];
-        }
-    }
-    return {};
-}
-
-const getTags = () => getContent(3);
-const getTag = (id) => {
-    const tagsArr = getTags();
-    for (let i = 0; i < tagsArr.length; i++) {
-        if (tagsArr[i].id === +id) {
-            return tagsArr[i];
-        }
-    }
-    return {};
-}
-
-const users = data.users;
-// only two users so lets wait till it gets more complex and we generate more data
-
-// Helper function to find one user based on id passed in url
-const findUser = (user_arr, id) => {
-    for (let i = 0; i < user_arr.length; i++) {
-        if (user_arr[i].id === id)
-            return user_arr[i];
-    }
-    return {};
-}
 export const fetchAll = async (url, params, method = "get") => {
-    switch (url) {
-        case "/api/pages":
-            return getPages()
-        case "/api/categories":
-            return getCategories()
-        case "/api/tags":
-            return getTags()
-        case "/api/users":
-            return users;
-    }
+    return await axios({
+        method, url: `http://localhost:3500${url}`, params
+    })
 }
 
 export const fetchOne = async (url, params = {}, method = "get") => {
-    const tempUrl = url.split('/');
-    const id = +tempUrl[tempUrl.length - 1];
-    const singleUser = findUser(users, id);
-    switch (tempUrl[tempUrl.length - 2]) {
-        case "pages":
-            return getPage(id)
-        case "categories":
-            return getCategory(id)
-        case "tags":
-            return getTag(id)
-        case "users":
-            return singleUser
-    }
+    return await axios({
+        method, url: `http://localhost:3500${url}`, params
+    })
 }
+
+
+export const save = async (url, data, method = "post") => {
+    return await axios({
+        method, url: `http://localhost:3500${url}`, data,
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+}
+
+
+
+
+// const data = require("../../output.json");
+
+// console.log(data.content.filter(it => it.contentTypeId === 1));
+
+// const getContent = (type) => data.content.filter(it => it.contentTypeId === +type);
+
+// const getPages = () => getContent(1);
+// const getPage = (id) => {
+//     const pagesArr = getPages();
+//     for (let i = 0; i < pagesArr.length; i++) {
+//         if (pagesArr[i].id === +id) {
+//             return pagesArr[i];
+//         }
+//     }
+//     return {};
+// }
+
+// const getCategories = () => getContent(2);
+// const getCategory = (id) => {
+//     const categoriesArr = getCategories();
+//     for (let i = 0; i < categoriesArr.length; i++) {
+//         if (categoriesArr[i].id === +id) {
+//             return categoriesArr[i];
+//         }
+//     }
+//     return {};
+// }
+
+// const getTags = () => getContent(3);
+// const getTag = (id) => {
+//     const tagsArr = getTags();
+//     for (let i = 0; i < tagsArr.length; i++) {
+//         if (tagsArr[i].id === +id) {
+//             return tagsArr[i];
+//         }
+//     }
+//     return {};
+// }
+
+// const users = data.users;
+// // only two users so lets wait till it gets more complex and we generate more data
+
+// // Helper function to find one user based on id passed in url
+// const findUser = (user_arr, id) => {
+//     for (let i = 0; i < user_arr.length; i++) {
+//         if (user_arr[i].id === id)
+//             return user_arr[i];
+//     }
+//     return {};
+// }
+// export const fetchAll = async (url, params, method = "get") => {
+//     switch (url) {
+//         case "/api/pages":
+//             return getPages()
+//         case "/api/categories":
+//             return getCategories()
+//         case "/api/tags":
+//             return getTags()
+//         case "/api/users":
+//             return users;
+//     }
+// }
+
+// export const fetchOne = async (url, params = {}, method = "get") => {
+//     const tempUrl = url.split('/');
+//     const id = +tempUrl[tempUrl.length - 1];
+//     const singleUser = findUser(users, id);
+//     switch (tempUrl[tempUrl.length - 2]) {
+//         case "pages":
+//             return getPage(id)
+//         case "categories":
+//             return getCategory(id)
+//         case "tags":
+//             return getTag(id)
+//         case "users":
+//             return singleUser
+//     }
+// }
 
--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,3 +1,11 @@
-const db = require("../models");
+const db = require("../../orm/models");
 
-const ContentController
+const ContentController = require("./ContentController");
+const UserController = require("./UserController");
+const ContentTypeController = require("./ContentTypeController");
+
+module.exports = {
+    ContentController: new ContentController(db, "Content"),
+    UserController: new UserController(db, "User"),
+    ContentTypeController: new ContentTypeController(db, "ContentType")
+}
