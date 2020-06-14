0aec84129b17b695a45f5e40ab669ea11c577914
Alaa <ahaddabeh@gmail.com>
2020-06-14 00:48:31 -0400

    Updated server.js file

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = requrie("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const handlebars = require("express-handlebars"); // I think
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3500;
-
+const HOST = process.env.HOST || "127.0.0.1";
 const app = express();
 
 // body parser parses json and non-json requests extracting params...
@@ -19,9 +20,12 @@ app.get("/", (req, res) => {
     res.send("It's working...");
 })
 
+// in the require, we are essentially pulling all the routes declared in the router
 app.use("/api/users", require("./src/api/routes/private/users"));
+app.use("/api/pages", require("./src/api/routes/private/pages"));
+app.use("/api/categories", require("./src/api/routes/private/categories"));
+app.use("/api/tags", require("./src/api/routes/private/tags"));
+app.use("/api/content", require("./src/api/routes/private/content"));
+app.use("/api/content-types", require("./src/api/routes/private/contentTypes"));
 
-// Ask about this because it's not gonna be /api/content it would be either pages, categories, or tags rather than content and i dont know where to get that from
-// app.use("/api/content", require("./src/api/routes/private/content"));
-
-app.listen(PORT, () => console.log(`JSON app is running on port::${PORT}`))
+app.listen(PORT, HOST, () => console.log(`JSON app is running on port::http://${HOST}:${PORT}`))
